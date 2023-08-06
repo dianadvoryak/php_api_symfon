27902bf0324b46b4f065bdbeae367765fabb3838
Diana Dvoryak <dianadvoryak.w@gmail.com>
2023-08-06 19:05:04 +0200

    fig exception handling

--- src/Model/ErrorResponse.php
+++ src/Model/ErrorResponse.php
@@ -4,7 +4,7 @@ namespace App\Model;
 
 class ErrorResponse
 {
-    public function __construct(private string $message)
+    public function __construct(private string $message, private mixed $details = null)
     {
     }
 
@@ -12,4 +12,10 @@ class ErrorResponse
     {
         return $this->message;
     }
+
+    public function getDetails(): mixed
+    {
+        return $this->details;
+    }
+
 }
